fd104b27818e0caf2dbb082fffa12b26d8860f44
Essam Al Joubori <essam.aljoubori@gmail.com>
2013-10-25 11:39:04 -0700

    First commit.

---
+++ personal/app/controllers/pages_controller.rb
@@ -0,0 +1,15 @@
+class PagesController < ApplicationController
+
+
+	  def home
+	  	
+	  end
+
+	  def portfolio
+	  end
+
+	  def contact
+	  end
+
+
+end
---
+++ personal/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Personal::Application.config.secret_key_base = 'ca996da65e9ac79bb8e4d16cb3951afe9bdd5d312dbf3c1686bb89926a0278567d8fc69c5e00855d21cd9a97875074401d90df17b3b5416c8b672f4d840b66d7'
